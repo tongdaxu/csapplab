25c2d338cd4fd9b20c1ecd988eb86e2b8817855a
x.tongda <x.tongda@nyu.edu>
2020-12-19 10:50:30 +0800

    bomblab 6

--- 1.bomb/bomb.asm
+++ 1.bomb/bomb.asm
@@ -509,68 +509,82 @@ Disassembly of section .text:
   4010f3:	c3                   	retq   
 
 00000000004010f4 <phase_6>:
-  4010f4:	41 56                	push   %r14
+  4010f4:	41 56                	push   %r14 ;callee side saving
   4010f6:	41 55                	push   %r13
   4010f8:	41 54                	push   %r12
   4010fa:	55                   	push   %rbp
   4010fb:	53                   	push   %rbx
-  4010fc:	48 83 ec 50          	sub    $0x50,%rsp
+  4010fc:	48 83 ec 50          	sub    $0x50,%rsp ; why 60?
   401100:	49 89 e5             	mov    %rsp,%r13
-  401103:	48 89 e6             	mov    %rsp,%rsi
+  401103:	48 89 e6             	mov    %rsp,%rsi ;2nd argument
   401106:	e8 51 03 00 00       	callq  40145c <read_six_numbers>
-  40110b:	49 89 e6             	mov    %rsp,%r14
-  40110e:	41 bc 00 00 00 00    	mov    $0x0,%r12d
+  40110b:	49 89 e6             	mov    %rsp,%r14 ;rsp, rbp, r13, r14 6 integers
+  40110e:	41 bc 00 00 00 00    	mov    $0x0,%r12d ;r12 a counter
+  ; loop to ensure every number <= 6 and != each other
   401114:	4c 89 ed             	mov    %r13,%rbp
-  401117:	41 8b 45 00          	mov    0x0(%r13),%eax
-  40111b:	83 e8 01             	sub    $0x1,%eax
+  401117:	41 8b 45 00          	mov    0x0(%r13),%eax ; first number!
+  40111b:	83 e8 01             	sub    $0x1,%eax 
   40111e:	83 f8 05             	cmp    $0x5,%eax
-  401121:	76 05                	jbe    401128 <phase_6+0x34>
+  401121:	76 05                	jbe    401128 <phase_6+0x34> ; every number <= 6, else explode
   401123:	e8 12 03 00 00       	callq  40143a <explode_bomb>
   401128:	41 83 c4 01          	add    $0x1,%r12d
   40112c:	41 83 fc 06          	cmp    $0x6,%r12d
-  401130:	74 21                	je     401153 <phase_6+0x5f>
+  401130:	74 21                	je     401153 <phase_6+0x5f> ; r12 != 6, do 
   401132:	44 89 e3             	mov    %r12d,%ebx
+  ; ---> loop to ensure number i!= rest of numbers
   401135:	48 63 c3             	movslq %ebx,%rax
-  401138:	8b 04 84             	mov    (%rsp,%rax,4),%eax
+  401138:	8b 04 84             	mov    (%rsp,%rax,4),%eax ; eax: second value
   40113b:	39 45 00             	cmp    %eax,0x0(%rbp)
-  40113e:	75 05                	jne    401145 <phase_6+0x51>
+  40113e:	75 05                	jne    401145 <phase_6+0x51> ; jump if second value != fist value
   401140:	e8 f5 02 00 00       	callq  40143a <explode_bomb>
   401145:	83 c3 01             	add    $0x1,%ebx
   401148:	83 fb 05             	cmp    $0x5,%ebx
   40114b:	7e e8                	jle    401135 <phase_6+0x41>
+  ; ---> loop end
   40114d:	49 83 c5 04          	add    $0x4,%r13
   401151:	eb c1                	jmp    401114 <phase_6+0x20>
-  401153:	48 8d 74 24 18       	lea    0x18(%rsp),%rsi
-  401158:	4c 89 f0             	mov    %r14,%rax
-  40115b:	b9 07 00 00 00       	mov    $0x7,%ecx
-  401160:	89 ca                	mov    %ecx,%edx
-  401162:	2b 10                	sub    (%rax),%edx
-  401164:	89 10                	mov    %edx,(%rax)
+  ; loop end
+
+  401153:	48 8d 74 24 18       	lea    0x18(%rsp),%rsi ; rsi = rsp + 24 (after actual 7 - i)
+  401158:	4c 89 f0             	mov    %r14,%rax ; rax = rsp
+  40115b:	b9 07 00 00 00       	mov    $0x7,%ecx ; ecx = 7
+  ; loop for i in numbers, number = 7 - number, 
+  401160:	89 ca                	mov    %ecx,%edx ; edx, ecx = constant 7
+  401162:	2b 10                	sub    (%rax),%edx ; edx -= (i th number)
+  401164:	89 10                	mov    %edx,(%rax) ; i th number = end
   401166:	48 83 c0 04          	add    $0x4,%rax
   40116a:	48 39 f0             	cmp    %rsi,%rax
   40116d:	75 f1                	jne    401160 <phase_6+0x6c>
-  40116f:	be 00 00 00 00       	mov    $0x0,%esi
+  ; loop end
+  40116f:	be 00 00 00 00       	mov    $0x0,%esi ; esi = 0
   401174:	eb 21                	jmp    401197 <phase_6+0xa3>
-  401176:	48 8b 52 08          	mov    0x8(%rdx),%rdx
+  ; loop
+  401176:	48 8b 52 08          	mov    0x8(%rdx),%rdx ; 0x6032d8 + i*16, could not exceed 5
   40117a:	83 c0 01             	add    $0x1,%eax
-  40117d:	39 c8                	cmp    %ecx,%eax
+  40117d:	39 c8                	cmp    %ecx,%eax #
   40117f:	75 f5                	jne    401176 <phase_6+0x82>
+  ; loop end
   401181:	eb 05                	jmp    401188 <phase_6+0x94>
+  ; ---> loop start, from the secocnd number find the number that is greater than 1 | less than 6
   401183:	ba d0 32 60 00       	mov    $0x6032d0,%edx
-  401188:	48 89 54 74 20       	mov    %rdx,0x20(%rsp,%rsi,2)
+  401188:	48 89 54 74 20       	mov    %rdx,0x20(%rsp,%rsi,2) ; write 32 @ rsp + 32 + i * 2
   40118d:	48 83 c6 04          	add    $0x4,%rsi
-  401191:	48 83 fe 18          	cmp    $0x18,%rsi
-  401195:	74 14                	je     4011ab <phase_6+0xb7>
-  401197:	8b 0c 34             	mov    (%rsp,%rsi,1),%ecx
+  401191:	48 83 fe 18          	cmp    $0x18,%rsi ; rsi is a counter used for 6 times
+  401195:	74 14                	je     4011ab <phase_6+0xb7> ; jump out condition
+  401197:	8b 0c 34             	mov    (%rsp,%rsi,1),%ecx ; rsi / 4 th number
   40119a:	83 f9 01             	cmp    $0x1,%ecx
-  40119d:	7e e4                	jle    401183 <phase_6+0x8f>
+  40119d:	7e e4                	jle    401183 <phase_6+0x8f> ; if ecx <= 1, loop to next number
+  ; ---> loop end
   40119f:	b8 01 00 00 00       	mov    $0x1,%eax
   4011a4:	ba d0 32 60 00       	mov    $0x6032d0,%edx
   4011a9:	eb cb                	jmp    401176 <phase_6+0x82>
+  ; 1: 6304464, 2: 6304480, 3: 6304496, 4: 6304512, 5: 6304528, 6: 6304544
+
   4011ab:	48 8b 5c 24 20       	mov    0x20(%rsp),%rbx
-  4011b0:	48 8d 44 24 28       	lea    0x28(%rsp),%rax
-  4011b5:	48 8d 74 24 50       	lea    0x50(%rsp),%rsi
-  4011ba:	48 89 d9             	mov    %rbx,%rcx
+  4011b0:	48 8d 44 24 28       	lea    0x28(%rsp),%rax ; +40
+  4011b5:	48 8d 74 24 50       	lea    0x50(%rsp),%rsi ; +80
+  4011ba:	48 89 d9             	mov    %rbx,%rcx       ; rsp + 32
+  ;loop start: this will loop for 5 times
   4011bd:	48 8b 10             	mov    (%rax),%rdx
   4011c0:	48 89 51 08          	mov    %rdx,0x8(%rcx)
   4011c4:	48 83 c0 08          	add    $0x8,%rax
@@ -578,6 +592,7 @@ Disassembly of section .text:
   4011cb:	74 05                	je     4011d2 <phase_6+0xde>
   4011cd:	48 89 d1             	mov    %rdx,%rcx
   4011d0:	eb eb                	jmp    4011bd <phase_6+0xc9>
+  ;loop end
   4011d2:	48 c7 42 08 00 00 00 	movq   $0x0,0x8(%rdx)
   4011d9:	00 
   4011da:	bd 05 00 00 00       	mov    $0x5,%ebp
@@ -801,18 +816,21 @@ Disassembly of section .text:
   401452:	bf 08 00 00 00       	mov    $0x8,%edi
   401457:	e8 c4 f7 ff ff       	callq  400c20 <exit@plt>
 
+; int read_six_numbers (char* input, char* destination) -> number of success
+; char * input -> rdi
+; char * dest  -> rsi
 000000000040145c <read_six_numbers>:
   40145c:	48 83 ec 18          	sub    $0x18,%rsp
-  401460:	48 89 f2             	mov    %rsi,%rdx
-  401463:	48 8d 4e 04          	lea    0x4(%rsi),%rcx
-  401467:	48 8d 46 14          	lea    0x14(%rsi),%rax
-  40146b:	48 89 44 24 08       	mov    %rax,0x8(%rsp)
+  401460:	48 89 f2             	mov    %rsi,%rdx       ;3rd
+  401463:	48 8d 4e 04          	lea    0x4(%rsi),%rcx  ;4th
+  401467:	48 8d 46 14          	lea    0x14(%rsi),%rax ;
+  40146b:	48 89 44 24 08       	mov    %rax,0x8(%rsp)  ;8th  
   401470:	48 8d 46 10          	lea    0x10(%rsi),%rax
-  401474:	48 89 04 24          	mov    %rax,(%rsp)
-  401478:	4c 8d 4e 0c          	lea    0xc(%rsi),%r9
-  40147c:	4c 8d 46 08          	lea    0x8(%rsi),%r8
-  401480:	be c3 25 40 00       	mov    $0x4025c3,%esi
-  401485:	b8 00 00 00 00       	mov    $0x0,%eax
+  401474:	48 89 04 24          	mov    %rax,(%rsp)     ;7th
+  401478:	4c 8d 4e 0c          	lea    0xc(%rsi),%r9   ;6th
+  40147c:	4c 8d 46 08          	lea    0x8(%rsi),%r8   ;5th
+  401480:	be c3 25 40 00       	mov    $0x4025c3,%esi  ;2nd string literal %d %d %d %d %d %d
+  401485:	b8 00 00 00 00       	mov    $0x0,%eax       ;1st rdi is the same as caller
   40148a:	e8 61 f7 ff ff       	callq  400bf0 <__isoc99_sscanf@plt>
   40148f:	83 f8 05             	cmp    $0x5,%eax
   401492:	7f 05                	jg     401499 <read_six_numbers+0x3d>
